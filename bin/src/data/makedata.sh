e2f7459c746ea00ae991951412688589c88e347f
svn <svn@ubuntu1204.com>
2014-05-27 21:19:52 +0800

    删除不必要的中间文件

--- bin/src/data/makedata.sh
+++
@@ -1,8 +0,0 @@
-#!bin/sh
-
-if [ "$1" == "" ]; then
-	echo "Need one argument."
-	exit
-fi
-
-echo $1 | ./datamaker > data
--- bin/src/db/sql.sh
+++
@@ -1,3 +0,0 @@
-#!bin/sh
-
-gcc -g -c  -o sql $(mysql_config --cflags) sql.c $(mysql_config --libs) datetime.o
